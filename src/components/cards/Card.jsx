a3d74e03091901ee4bb8f939da691c77c075ba2e
veyseltosun <postalarim1923@gmail.com>
2022-12-08 11:52:47 +0300

    card jsx start ing dev.

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { data } from "../../helper/data";
+import "./Card.css";
+
+
 
 function Card() {
   return (
-    <div>Card</div>
+    <div className='card-container'>
+      <div className='cards'>
+        <h2>{data[0].title}</h2>
+        <img src={data[0].image } alt={data[0].title}/>
+        <p>{data[0].desc}</p>
+
+
+      </div>
+
+    </div>
   )
 }
 
