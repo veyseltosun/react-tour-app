5b8b3d17dd59b6792e1da7bbc1eaa4a55e2417ec
veyseltosun <postalarim1923@gmail.com>
2022-12-08 11:57:22 +0300

    cards

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -8,9 +8,17 @@ function Card() {
   return (
     <div className='card-container'>
       <div className='cards'>
-        <h2>{data[0].title}</h2>
-        <img src={data[0].image } alt={data[0].title}/>
-        <p>{data[0].desc}</p>
+        <div className='title'>
+
+          <h2>{data[0].title}</h2>
+
+        </div>
+
+        <img src={data[0].image} alt={data[0].title} />
+        
+        <div className='card-over'>
+          <p>{data[0].desc}</p>
+        </div>
 
 
       </div>
