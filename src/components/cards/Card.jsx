1b18db038e0ad4260a14051c80f5a4510f2ab75c
veyseltosun <postalarim1923@gmail.com>
2022-12-08 14:09:59 +0300

    last before finish ı guess

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,30 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { data } from "../../helper/data";
 import "./Card.css";
 
 
 
 function Card() {
+  const [showImage, setShowImage] = useState(true)
   return (
-    <div className='card-container'>
+    <div className='card-container' onClick={() => setShowImage(!showImage)}>
+      {showImage ? (<>
+        {data.map((card) => {
+          const { id, desc, title, image } = card
+          return (
+            <div className='cards' key={id}>
+              
+              <img src={image} alt={title} />
 
-      {data.map((card)=>{
-        const{id, desc, title, image} = card
-        return(
-          <div className='cards' key={id}>
-            <div className='title'>
-              <h2>{title}</h2>
-            </div>
-            <img src={image} alt={title}/>
             
-            <div className="card-over">
-              <p>{desc}</p>
             </div>
+          )
+        })}
+      </>
+      ) : (<>
+        {data.map((card) => {
+          const { id, desc, title, image } = card
+          return (
+            <div className='cards' key={id}>
+              <div className='title'>
+                <h2>{title}</h2>
+              </div>
+              <img src={image} alt={title} />
 
-          </div>
-        )
-      })}
-      
+              <div className="card-over">
+                <p>{desc}</p>
+              </div>
+
+            </div>
+          )
+        })}
+      </>
+      )}
+      <>
+        {data.map((card) => {
+          const { id, desc, title, image } = card
+          return (
+            <div className='cards' key={id}>
+              <div className='title'>
+                <h2>{title}</h2>
+              </div>
+              <img src={image} alt={title} />
+
+              <div className="card-over">
+                <p>{desc}</p>
+              </div>
+
+            </div>
+          )
+        })}
+      </>
+
 
     </div>
   )
