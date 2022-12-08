44a214122926149aca5d14157644d77639889d9f
veyseltosun <postalarim1923@gmail.com>
2022-12-08 10:00:03 +0300

    initial commit.

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# react-tour-app
+simple react app. 
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Card from './components/cards/Card';
+import Header from './components/header/Header';
+import Navbar from './components/navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+    <Header/>
+    <Navbar/>
+    <Card/>
+
+    </div>
+  );
+}
+
+export default App;
