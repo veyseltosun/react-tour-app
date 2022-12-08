3d5d063c0a430fef6391a588d2d74c589e4ee6e5
veyseltosun <postalarim1923@gmail.com>
2022-12-08 10:31:30 +0300

    navbar.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+      <a href='aboutus'>ABOUT US</a>
+      <a href='foryou'>FOR YOU</a>
+      <a href='services'>SERVICES</a>
+      <a href='blog'>BLOG</a>
+      <a href='vlog'>VLOG</a>
+      <a href='contact'>CONTACT</a>
+    </div>
   )
 }
 
